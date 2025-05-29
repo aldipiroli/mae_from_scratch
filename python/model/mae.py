a667593c016cc8075fd0a39feb554114becec3d0
Aldi Piroli <aldipiroli@gmail.com>
2025-05-29 17:46:57 +0200

    minor fixes in model

--- python/model/mae.py
+++ python/model/mae.py
@@ -67,17 +67,17 @@ class MAE(nn.Module):
         patch_kernel_size=8,
         img_size=(3, 64, 64),
         embed_size=256,
-        mask_fraction=0.75,
-        encoder_num_transformer_blocks=12,
-        encoder_num_attention_heads=8,
-        decoder_num_transformer_blocks=12,
-        decoder_num_attention_heads=8,
+        mask_fraction=0.0,
+        encoder_num_transformer_blocks=2,
+        encoder_num_attention_heads=2,
+        decoder_num_transformer_blocks=2,
+        decoder_num_attention_heads=2,
     ):
         super(MAE, self).__init__()
-        self.encoder_num_transformer_blocks = encoder_num_transformer_blocks
         self.encoder_num_attention_heads = encoder_num_attention_heads
-        self.decoder_num_transformer_blocks = decoder_num_transformer_blocks
+        self.encoder_num_transformer_blocks = encoder_num_transformer_blocks
         self.decoder_num_attention_heads = decoder_num_attention_heads
+        self.decoder_num_transformer_blocks = decoder_num_transformer_blocks
 
         self.patch_kernel_size = patch_kernel_size
         self.img_size = img_size
@@ -111,10 +111,8 @@ class MAE(nn.Module):
         )
 
         self.embed_mask = EmbedMasking(mask_fraction=self.mask_fraction)
-        self.mask_tokens = nn.Parameter(
-            data=torch.zeros(int(self.num_patches * (self.mask_fraction)), self.embed_size),
-            requires_grad=True,
-        )
+        self.mask_tokens = nn.Parameter(torch.zeros(1, 1, self.embed_size), requires_grad=True)
+
         self.embed_patches_for_decoder = EmbedPatches(
             patch_dim=self.embed_size,
             embed_size=self.embed_size,
@@ -130,11 +128,11 @@ class MAE(nn.Module):
         x_masked, random_indices = self.embed_mask(x_embed)
         x_encoder = self.transformer_encoder(x_masked)
 
-        mask_tokens = self.mask_tokens.unsqueeze(0).expand(b, -1, -1)
+        mask_tokens = self.mask_tokens.expand(b, self.num_patches, -1)
         x_encoder_w_masked_tokens = torch.cat([x_encoder, mask_tokens], 1)
-        pred_token_mask = torch.ones((b, self.num_patches), device=x.device)
+
+        pred_token_mask = torch.ones((b, self.num_patches, self.embed_size), device=x.device)
         pred_token_mask[:, : x_encoder.shape[1]] = 0
-        pred_token_mask = pred_token_mask.unsqueeze(-1).expand(-1, -1, self.embed_size)
 
         x_unshuffle = self.unshuffle_tokens(x_encoder_w_masked_tokens, random_indices)
         pred_token_mask_unshuffle = self.unshuffle_tokens(pred_token_mask, random_indices)
