656e3cecb3e15e1cf6469dfadc8e288d77a17996
Aldi Piroli <aldipiroli@gmail.com>
2025-05-29 12:42:40 +0200

    Update tests

--- python/test/test_mae.py
+++ python/test/test_mae.py
@@ -41,10 +41,19 @@ def test_patch_and_unshuffle():
     x_embed, random_indices = embed_mask(x_patch)
     mask_tokens = torch.zeros(b, 48, d)
 
+    pred_token_mask = torch.ones((b, n))
+    pred_token_mask[:, : x_embed.shape[1]] = 0
+    pred_token_mask = pred_token_mask.unsqueeze(-1).expand(-1, -1, d)
+
     x_encoder_w_masked_tokens = torch.cat([x_embed, mask_tokens], 1)
+
     x_unshuffle = mae.unshuffle_tokens(x_encoder_w_masked_tokens, random_indices)
+    pred_token_mask_unshuffle = mae.unshuffle_tokens(pred_token_mask, random_indices)
+
     x_fold = patch_image.fold(x_unshuffle, (h, w))
+    pred_token_mask_fold = patch_image.fold(pred_token_mask_unshuffle, (h, w))
     assert x_fold.shape == (b, c, h, w)
+    assert pred_token_mask_fold.shape == (b, c, h, w)
 
 
 def test_mae_forward_pass():
@@ -54,7 +63,7 @@ def test_mae_forward_pass():
     x = torch.randn(b, c, h, w)
     output_dict = mae(x)
     assert output_dict["pixel_preds"].shape == (b, n, d)
-    assert output_dict["pred_token_mask"].shape == (b, n, d)
+    assert output_dict["pred_token_mask"].shape == (b, n)
 
 
 if __name__ == "__main__":
