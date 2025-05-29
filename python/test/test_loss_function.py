f8ff07e3c05c16e7387ed42c412fa815ba135af8
Aldi Piroli <aldipiroli@gmail.com>
2025-05-29 10:03:58 +0200

    Fixed test loss function

--- python/test/test_loss_function.py
+++ python/test/test_loss_function.py
@@ -7,15 +7,14 @@ import torch
 
 
 def test_pixel_reconstruction_loss():
-    b, c, h, w = 8, 3, 64, 64
-    x = torch.randn(b, c, h, w)
-    y = torch.randn(b, c, h, w)
-    mask = torch.ones(b, c, h, w).bool()
-    mask[:, :, :, :10] = 0
+    b, n, d = 8, 64, 192
+    x = torch.randn(b, n, d)
+    y = torch.randn(b, n, d)
+    mask = torch.ones(b, n, d).bool()
+    mask[:, :10, :] = 0
     loss_fn = PixelReconstructionLoss()
     loss = loss_fn(x, y, mask)
     assert loss > 0
-
 
 if __name__ == "__main__":
     test_pixel_reconstruction_loss()
